4eed5827677309a39661d9c044939f4a25da80ba
SelimMeske <mesic.selim@outlook.com>
2020-12-27 16:06:09 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,168 @@
+from google_trans_new import google_translator
+import re
+
+example_text = """
+Jigsaw makes <a href="https://top-buy.net/best-laser-measuring-tools-reviews/" target="_blank" rel="noopener noreferrer">engineering</a> material into different shapes easier. Jigsaw can <strong>cut metals, laminate, PVC, and wood</strong>. This tool can make cutting these materials straight or curved, but its common use is making curved designs.
+<h2>Best Jigsaws in Comparison</h2>
+[ARPrice id=305] [ads2]
+
+[embed]https://www.youtube.com/watch?v=f1cuvdA99tM[/embed]
+
+[toc]
+<h2>What is a Jigsaw</h2>
+[box type="info" align="" class="" width=""]It was said to have emerged in the 19th century, but they do not use it without the help of a treadle. The <strong>modern portable jigsaw</strong> didn't come out until 1947 when it was introduced by Scintilla AG, which was later on acquired by the company Bosch.[/box]
+
+Jigsaw can efficiently cut angles up to 45 degrees in making miter joints.
+<h2>Jigsaw and how they are used?</h2>
+<img class="size-medium wp-image-3631 alignright" src="https://top-buy.net/wp-content/uploads/2020/09/Jigsaw-1-300x277.jpg" alt="Jigsaw and how they are used?" width="300" height="277" />Jigsaws are used by<strong> attaching different blade accessory</strong> to it. Bosch was able to make using a jigsaw easier by manufacturing a tool-free blade change system. Previous jigsaw models require a blade screwed on the tool, which complicates using it a bit. Blades include T-shank blades and U-shank blades. The tooth design is also important for the efficiency of this tool.
+
+You can choose a side set and the ground tooth<strong> to provide clean and fast cuts</strong> in plastics and wood while the wavy set and milled teeth can cut through metal, plastic, and aluminum. They introduce another material they can use with the jigsaw, which is the reduced-kerf carbide and diamond grit edges to help cut through hard materials and make the work faster. Using a jigsaw requires knowledge and will need a few trials before you can get it smoothly cutting materials that you like.
+
+Blades of the jigsaw are weak and small since the lower end is <strong>unsupported.</strong> To make sure you get the cut you prefer, they use a blade roller, this will keep the blade aligned above the sole plate. Guiding a jigsaw is not by forcing it to move, but you must steer it, and move sideways instead. Making sure that you have sharp blades all the time is important to get <strong>high-quality cuts.</strong>
+
+<strong>PROS</strong>
+
+[tie_list type="checklist"]
+<ul>
+ 	<li>Jigsaws are a <strong>very versatile carpentry tool.</strong> If you have it in the house, you can use it for almost anything you need to cut through, and you will always find it handy.</li>
+ 	<li>It can cut through original materials such as metal, wood, aluminum, PVC, foam, and plastic. You can have an ordinary saw and cut materials, using it, but you can't find the versatility of this tool with another.</li>
+ 	<li>You can store it using just a small space. It does not require vast spaces, unlike other tools in carpentry. It is also highly portable that you can move it anywhere inside your shop or even your garage.</li>
+ 	<li>It is <strong>lightweight,</strong> and you can bring it with you anywhere to help you with your work.</li>
+ 	<li>You can use this tool in your home for improvements, and you can also use it in your workshop, and you will not have a hard time finding a use for a jigsaw.</li>
+</ul>
+<span class="fontstyle0">[/tie_list]</span>
+
+<strong>CONS</strong>
+
+[tie_list type="cons"]
+<ul>
+ 	<li>Since jigsaw is a handheld saw<strong>, it may not be too accurate</strong>. The cuts may not be fine as well, with some, obvious edges or rough points.</li>
+ 	<li>The efficiency of the cut may also depend on how the user handles this tool.</li>
+</ul>
+<span class="fontstyle0">[/tie_list]</span>
+<h2>What types of Jigsaw are there?</h2>
+<img class="size-medium wp-image-3630 alignleft" src="https://top-buy.net/wp-content/uploads/2020/09/Jigsaw-7-300x284.jpg" alt="What types of Jigsaw are there?" width="300" height="284" />There is a corded jigsaw and a cordless jigsaw.
+
+<strong>Corded</strong> - First introduced type of jigsaw with a cord and where the power is from the use of the main electric source where you usually plug it in. Corded jigsaw may differ with the power and energy it can give out. This type, though, may need getting used to since there is a cord that you need to consider while working. It is also not very portable to bring with you anywhere.
+
+<strong>Cordless jigsaw</strong> - Its power is from the battery and, once used up, requires charging. This jigsaw is lightweight and portable, as it is meant to be lighter compared to the corded jigsaw for easy maneuverability. You can bring it with you anywhere without worrying where you need to plug it in as long as you have it fully charged.
+
+Cordless jigsaw is less powerful as compared to the corded one.
+
+<strong>Types of jigsaw according to blades</strong>
+<ol>
+ 	<li><strong>Carbon steel Jigsaw blade -</strong> this blade is the most common and can cut through wood and PVC pipes.</li>
+ 	<li><strong>Bi-Metal Jigsaw Blade</strong> - this blade has hardened teeth and is considered more powerful to cut through metal.</li>
+ 	<li><strong>Carbide Jigsaw Blades</strong> - this blade, on the other hand, can cut through different materials and is considered more powerful and fast.</li>
+</ol>
+<h2>This is what we check in Jigsaw Reviews</h2>
+<img class="size-medium wp-image-3629 alignright" src="https://top-buy.net/wp-content/uploads/2020/09/Jigsaw-6-300x295.jpg" alt="This is what we check in Jigsaw Reviews" width="300" height="295" />They invent jigsaws to make <strong>cutting materials fast and effortless</strong>. This will shorten the time of work instead of using the manual tools used previously. However, with tools such as this, we also need to review the materials used in manufacturing, its longevity or durability, the price of the product, and the cleaning process.
+<h3>Materials</h3>
+Mostly made of stainless steel material plus the blade used can be made of <strong>high-carbon steel</strong> or <strong>high-speed steel.</strong> The bi-metal blade contains both high-carbon steel and high-speed materials. When combined, this will reinforce the tool strongly and creates a blade that has less risk of breakage and has high flexibility plus versatility. Bi-metal has a longer lifespan compared to other blade types.
+
+There is also the material Tungsten carbide, which can cut through any material while <strong>Diamond-grit blades are highly versatile.</strong> Users can decide which to choose, and the length of use will depend on the material as well.
+<h3>Longevity</h3>
+Tungsten carbide and <strong>Diamond-grit blades</strong> are more durable and highly preferred by more users.
+<h3>Price</h3>
+The price of the jigsaw will depend on stroke, length, power, and speed. Plus, the blade used and as <strong>many features,</strong> you choose the higher-priced the jigsaw will be.
+<h3>Cleaning</h3>
+<ul>
+ 	<li>Cleaning the jigsaw is preferred every after use and every replacement of pieces.</li>
+ 	<li><strong>Keep the jigsaw free</strong> from dust and debris so your jigsaw will not overheat while in use.</li>
+ 	<li>The handle and other parts must be clean and free of grease so that when handling it, there is no risk of accidents.</li>
+ 	<li>Removing dirt or grease is done with a clean cloth plus mild soap.</li>
+ 	<li><strong>Blade guide needs cleaning,</strong> also where an old toothbrush can remove grime and dirt at the same time.</li>
+ 	<li><strong>Lubrication</strong> is important because your jigsaw will not work efficiently without proper lubrication. [ads2]</li>
+</ul>
+<h2>Buying the best Jigsaw</h2>
+<img class="size-medium wp-image-3628 alignright" src="https://top-buy.net/wp-content/uploads/2020/09/Jigsaw-5-300x253.jpg" alt="Buying the best Jigsaw" width="300" height="253" />With the availability of different branded jigsaw tool, you will have a hard time finding the best one for your needs. Most especially if you are not familiar with it, you can get confused with the features and some terms about the tool.
+Here are some reminders when you are b<strong>uying a jigsaw.</strong>
+<h3>Instore</h3>
+<ul>
+ 	<li>When you look for a jigsaw, you need to know the <strong>SPM or strokes per minute,</strong> which is the measurement of the maximum speed that is measured in strokes. Or it can be the fastness of the blade which completes a cutting motion in a minute.</li>
+ 	<li>What amp rating will you need? If you need a jigsaw for light cutting work, then there is a 4-amp motor, which is also inexpensive, but for a higher quality of work, you will need a 7-amp or a higher model, which is also, more expensive.</li>
+ 	<li><strong>Would you like a corded or cordless jigsaw?</strong> If you are after the portability and convenience of use, then you can go for the cordless type, but if you have no issues working with a corded jigsaw, then get a jigsaw with cord since it will also have more power and is considered heavy duty.</li>
+ 	<li>Type of blade is another consideration, there are types of the blade, that may cost you more, but it will last you longer, plus it will give you the efficient cutting power that you are looking for.</li>
+ 	<li>This may not mean much if you are starting with using the tool, but for those who know more, there are 2-types of cutting action, which are straight or orbital. For those who are just after cutting a straight cut, then a straight cutting action is what you need while the orbital cutting action is chosen if you like to make delicate cuts.</li>
+ 	<li>[box type="info" align="" class="" width=""]Blades are something you need to consider, such as <strong>T-shank or U-shank blades.</strong> If you are to change blades often, you may want to consider a T-shank blade, which is easier to remove and install.[/box]</li>
+</ul>
+<h3>Online</h3>
+<ul>
+ 	<li>You can only check the specs online and also with the help of the online store seller.</li>
+ 	<li>Here are some pointers you need to remember when buying jigsaw online.</li>
+ 	<li>Some features may be a preference, while other features are given more priority.</li>
+ 	<li><strong>Ask about the variable speed</strong> of the jigsaw you are planning to buy. Contact the seller and ask what speeds of jigsaw do they currently have, at the same time, ask the prices for each so you can get the different brands they sell and compare it.</li>
+ 	<li>If you need to know which is better to use, whether it is a jigsaw with orbital action or the one with an up-and-down motion jigsaw. Some sellers will also let you in on the style on how you can handle a jigsaw tool well, even if you are just starting with its use.</li>
+ 	<li><strong>Price</strong> is for your high list of consideration since expensive jigsaw almost always has the features you are looking for, such as the high motor power, the depth of cut, there may also be a laser guideline features and some have a dust blower also which makes cleaning after use easier. <strong>Compare</strong> the prices of each jigsaw that is available at the online store you choose and pick the best one that will cater to your needs.</li>
+</ul>
+<h2>FAQ about the best Jigsaw</h2>
+<h3>What is the best Jigsaw that is available today?</h3>
+<ol>
+ 	<li><strong><img class="size-medium wp-image-3627 alignright" src="https://top-buy.net/wp-content/uploads/2020/09/Jigsaw-4-300x289.jpg" alt="What is the best Jigsaw that is available today?" width="300" height="289" />BOSCH JS470E</strong> - True value for your money, but less the portability. This cordless jigsaw gives you a 7-amp motor power that can cut through any types of materials. It is very convenient to use without the vibration you get from some power tools, but with all the accuracy and comfort you are looking for.</li>
+ 	<li><strong>MAKITA XVJ03Z</strong> - Affordable and, at the same time, portable. It has lesser power, but you can use it still for some home improvements and projects that you need to do. An excellent choice for professionals and amateurs alike.</li>
+ 	<li><strong>DEWALT DCS334B</strong> - Heavy duty with a powerful 20V Max battery. It can cut through intricate designs in any materials. This type can cut quicker and longer, plus its brushless motor will surely enhance its performance. It is a solid, compact, and efficient tool. It is worth the money you will spend.</li>
+ 	<li><strong>BLACK+DECKER BDCJS20C</strong> - If you need a tool that you can rely on at home, then this one is for you. It is powered by a 20V battery with a variable speed trigger. It is the best jigsaw for home use as you can finish your home improvements on time without the hassle. This brand is affordable and reliable.</li>
+ 	<li><strong>PORTER-CABLE</strong> <strong>20V MAX Jig Saw, PCC650B</strong> - If you are beginning to grasp the use of jigsaw tool, then this one is for you. You get to have more control over your cuts that will look cleaner with the variable speed feature this tool has. It also includes a T-Shank blade plus a tool-free blade release for easy blade changing. Not to mention the dust blower that is included, to make the post cutting task easier.</li>
+</ol>
+<h3>How do you store a jigsaw correctly?</h3>
+Some jigsaws come with a storage case that can protect the jigsaw while not in use. If in case your jigsaw does not have a storage case, make sure it is kept in a dry place away from the wet surface.
+
+[embed]https://www.youtube.com/watch?v=b3RB3Vq8yQI[/embed]
+<h2>Conclusion</h2>
+With the practicality and ease of use, a jigsaw can be <strong>one of the best tools there is</strong>. With its versatility, you can apply its use, to any materials that you have in your home during repair/improvement or professional carpentry works. You will not be left without an option because of the vast features that are available for different models or brands. [ads2]
+<h2>Links:</h2>
+<ul>
+ 	<li><a href="https://www.einhell.de/en-de/blog/workshop/straight-sawing-how-to-use-a-jigsaw.html" target="_blank" rel="noopener noreferrer">https://www.einhell.de/en-de/blog/workshop/straight-sawing-how-to-use-a-jigsaw.html</a></li>
+ 	<li><a href="https://www.sgs-engineering.com/help-advice/successfully-use-jigsaw/" target="_blank" rel="noopener noreferrer">https://www.sgs-engineering.com/help-advice/successfully-use-jigsaw/</a></li>
+ 	<li><a href="https://www.wikihow.com/Use-a-Jigsaw" target="_blank" rel="noopener noreferrer">https://www.wikihow.com/Use-a-Jigsaw</a></li>
+ 	<li><a href="https://www.bobvila.com/articles/how-to-use-a-jigsaw/" target="_blank" rel="noopener noreferrer">https://www.bobvila.com/articles/how-to-use-a-jigsaw/</a></li>
+</ul>
+"""
+
+
+
+class Translator_Jomi:
+    def __init__(self, text):
+        self.text = text
+    def clean_strong(self):
+        self.text = self.text.replace('<strong>', '').replace('</strong>', '')
+    def clean_a(self):
+        output = re.findall('<a href.*</a>', self.text, re.MULTILINE)
+        for i in output:
+            # Replace </a>
+            clean_i = i.replace('</a>', '')
+            find_a_start = re.match(r'<a href.*>', clean_i, re.MULTILINE)
+            clean_i = clean_i.replace(find_a_start.group(0), '')
+            self.text = self.text.replace(i, clean_i)
+    def translations(self):
+        splited_text = self.text.splitlines()
+        translator = google_translator()
+
+        for i in splited_text:
+            # Search for image
+            search_for_image = re.match(r'<img.*/>', i, re.MULTILINE)
+            # Search for box
+            search_for_box = re.match(r'\[box.*\[/box]', i, re.MULTILINE)
+
+            if search_for_image:
+                clean_image = i.replace(search_for_image.group(0), '')
+                translated = translator.translate(clean_image, lang_tgt='hr')
+                self.text = self.text.replace(clean_image, translated)
+            elif search_for_box:
+                clean_closing_box = i.replace('[/box]', '')
+                clean_opening_box = re.match(r'\[box.*]', i, re.MULTILINE)
+                clean_box = clean_closing_box.replace(clean_opening_box.group(0), '')
+                translated = translator.translate(clean_box, lang_tgt='hr')
+                self.text = self.text.replace(clean_box, translated)
+            else:
+                translated = translator.translate(i, lang_tgt='hr')
+                self.text = self.text.replace(i, translated)
+
+        self.text = self.text.replace('</ Ul>', '</ul>')
+        self.text = self.text.replace('<Ul>', '<ul>')
+text = Translator_Jomi(example_text)
+
+text.clean_a()
+text.clean_strong()
+text.translations()
+print(text.text)
